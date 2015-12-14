422926921553e8b5c5506f8566733d01858fdfa4
Den Patin <denpatin@gmail.com>
2015-12-14 19:32:51 +0300

    itunes.bat: Some optimizations.

--- itunes.bat
+++ itunes.bat
@@ -1,31 +1,20 @@
 REM Batch to start/stop iTunes with corresponding services^
 
+set ITUNES_SERVICES=("iPod Service" "Bonjour Service" "Apple Mobile Device Service")
+
 IF "%1" == "stop" (
-  taskkill /IM iTunes.exe
-  sc stop "iPod Service"
-  sc stop "Bonjour Service"
-  sc stop "Apple Mobile Device Service"
-  sc config "iPod Service" start= disabled
-  sc config "Bonjour Service" start= disabled
-  sc config "Apple Mobile Device Service" start= disabled
-  ECHO Successfully stopped all the iTunes related processes!
+  taskkill /IM "iTunes.exe"
+  FOR %%G IN %ITUNES_SERVICES% DO (
+    sc stop %%G
+    sc config %%G start= disabled
+  )
 ) ELSE (
-  FOR /F "tokens=3 delims=: " %%H in ('sc query "iPod Service" ^| findstr "        STATE"') do (
-    IF /I "%%H" NEQ "RUNNING" (
-      sc config "iPod Service" start= demand
-      sc start "iPod Service"
-    )
-  )
-  FOR /F "tokens=3 delims=: " %%H in ('sc query "Bonjour Service" ^| findstr "        STATE"') do (
-    IF /I "%%H" NEQ "RUNNING" (
-      sc config "Bonjour Service" start= demand
-      sc start "Bonjour Service"
-    )
-  )
-  FOR /F "tokens=3 delims=: " %%H in ('sc query "Apple Mobile Device Service" ^| findstr "        STATE"') do (
-    IF /I "%%H" NEQ "RUNNING" (
-      sc config "Apple Mobile Device Service" start= demand
-      sc start "Apple Mobile Device Service"
+  FOR %%G IN %ITUNES_SERVICES% DO (
+    FOR /F "tokens=3 delims=: " %%H in ('sc query %%G ^| findstr "        STATE"') do (
+      IF /I "%%H" NEQ "RUNNING" (
+        sc config %%G start= demand
+        sc start %%G
+      )
     )
   )
   START "iTunes" /D "C:\Program Files\iTunes\" "iTunes.exe"
